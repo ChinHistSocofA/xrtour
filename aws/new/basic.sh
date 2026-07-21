64d60efd8e700877f6c6468e95e21222f7098c9b
Francis Li <mail@francisli.com>
2026-07-21 14:21:42 -0700

    VPC stack working

--- aws/new/basic.sh
+++ aws/new/basic.sh
@@ -4,11 +4,21 @@ read -p "Stack base name (lowercase, letters, numbers, hyphen only): " BASE_NAME
 read -p "Domain to configure: " DOMAIN
 
 # create the stack
-aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name ${BASE_NAME}-vpc --template-body file://./vpc.json --parameters ParameterKey=BaseName,ParameterValue=$BASE_NAME ParameterKey=Domain,ParameterValue=$DOMAIN
+aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name ${BASE_NAME}-vpc --template-body file://./basic/vpc.json --parameters ParameterKey=BaseName,ParameterValue=$BASE_NAME
 
 # wait for completion
 aws cloudformation wait stack-create-complete --stack-name ${BASE_NAME}-vpc --output text
 
-# print the nameservers for the public hosted zone for the domain
-echo "Name Servers:"
-aws cloudformation describe-stacks --stack-name ${BASE_NAME}-vpc --query 'Stacks[0].Outputs[?OutputKey==`PublicHostedZoneNameServers`].OutputValue' --output text 
+# print the IP address
+echo "Elastic IP:"
+aws cloudformation describe-stacks --stack-name ${BASE_NAME}-vpc --query 'Stacks[0].Outputs[?OutputKey==`ElasticIP0PublicIP`].OutputValue' --output text
+
+echo "Assign DNS A records for your domain and wildcard subdomains before continuing:"
+echo $DOMAIN
+echo "*.$DOMAIN"
+echo "*.staging.$DOMAIN"
+
+read -p "Continue? [y/N]: " CONT
+if [[ "$CONT" != "y" ]]; then
+    exit 1
+fi
