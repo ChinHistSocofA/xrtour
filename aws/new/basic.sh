044bcf97c2eff80bd594317cfc08c9fa1b71a73a
Francis Li <mail@francisli.com>
2026-07-21 16:16:02 -0700

    Update vpc stack to include domain, implement ses stack creation

--- aws/new/basic.sh
+++ aws/new/basic.sh
@@ -1,24 +1,58 @@
 #!/bin/bash
 
-read -p "Stack base name (lowercase, letters, numbers, hyphen only): " BASE_NAME
-read -p "Domain to configure: " DOMAIN
+read -p "Stack base name (lowercase, letters, numbers, hyphen only): [$1]" BASE_NAME
+BASE_NAME=${BASE_NAME:-$1}
+read -p "Domain to configure: [$2]" DOMAIN
+DOMAIN=${DOMAIN:-$2}
 
-# create the stack
-aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name ${BASE_NAME}-vpc --template-body file://./basic/vpc.json --parameters ParameterKey=BaseName,ParameterValue=$BASE_NAME
+REGION=`aws configure get region`
 
-# wait for completion
-aws cloudformation wait stack-create-complete --stack-name ${BASE_NAME}-vpc --output text
+# check if vpc stack created
+if ! aws cloudformation describe-stacks --stack-name ${BASE_NAME}-vpc >/dev/null 2>&1; then
+  # if not, create the stack
+  echo "Creating ${BASE_NAME}-vpc stack"
+  aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name ${BASE_NAME}-vpc --template-body file://./basic/vpc.json --parameters ParameterKey=BaseName,ParameterValue=$BASE_NAME ParameterKey=Domain,ParameterValue=$DOMAIN
+  # wait for completion
+  aws cloudformation wait stack-create-complete --stack-name ${BASE_NAME}-vpc --output text
+  # print the IP address
+  echo "Elastic IP:"
+  aws cloudformation describe-stacks --stack-name ${BASE_NAME}-vpc --query 'Stacks[0].Outputs[?OutputKey==`ElasticIP0PublicIP`].OutputValue' --output text
+  echo
+  echo "Assign DNS A records for your domain and wildcard subdomains before continuing:"
+  echo
+  echo $DOMAIN
+  echo "*.$DOMAIN"
+  echo "*.staging.$DOMAIN"
+  echo
+  read -p "Continue? [y/N]: " CONT
+  if [[ "$CONT" != "y" ]]; then
+      exit 1
+  fi
+fi
 
-# print the IP address
-echo "Elastic IP:"
-aws cloudformation describe-stacks --stack-name ${BASE_NAME}-vpc --query 'Stacks[0].Outputs[?OutputKey==`ElasticIP0PublicIP`].OutputValue' --output text
-
-echo "Assign DNS A records for your domain and wildcard subdomains before continuing:"
-echo $DOMAIN
-echo "*.$DOMAIN"
-echo "*.staging.$DOMAIN"
-
-read -p "Continue? [y/N]: " CONT
-if [[ "$CONT" != "y" ]]; then
-    exit 1
+# check if ses stack created
+if ! aws cloudformation describe-stacks --stack-name ${BASE_NAME}-ses >/dev/null 2>&1; then
+  # create the stack
+  echo "Creating ${BASE_NAME}-ses stack"
+  aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name ${BASE_NAME}-ses --template-body file://./ses.json --parameters ParameterKey=BaseName,ParameterValue=$BASE_NAME
+  # wait for completion
+  aws cloudformation wait stack-create-complete --stack-name ${BASE_NAME}-ses --output text
+  echo "Assign DNS records for the following:"
+  echo
+  echo "TXT ses.$DOMAIN \"v=spf1 include:amazonses.com ~all\""
+  echo "MX ses.$DOMAIN 10 feedback-smtp.$REGION.amazonaws.com"
+  NAME1=`aws cloudformation describe-stacks --stack-name ${BASE_NAME}-ses --query 'Stacks[0].Outputs[?OutputKey==\`DkimDNSTokenName1\`].OutputValue' --output text`
+  VALUE1=`aws cloudformation describe-stacks --stack-name ${BASE_NAME}-ses --query 'Stacks[0].Outputs[?OutputKey==\`DkimDNSTokenValue1\`].OutputValue' --output text`
+  NAME2=`aws cloudformation describe-stacks --stack-name ${BASE_NAME}-ses --query 'Stacks[0].Outputs[?OutputKey==\`DkimDNSTokenName2\`].OutputValue' --output text`
+  VALUE2=`aws cloudformation describe-stacks --stack-name ${BASE_NAME}-ses --query 'Stacks[0].Outputs[?OutputKey==\`DkimDNSTokenValue2\`].OutputValue' --output text`
+  NAME3=`aws cloudformation describe-stacks --stack-name ${BASE_NAME}-ses --query 'Stacks[0].Outputs[?OutputKey==\`DkimDNSTokenName3\`].OutputValue' --output text`
+  VALUE3=`aws cloudformation describe-stacks --stack-name ${BASE_NAME}-ses --query 'Stacks[0].Outputs[?OutputKey==\`DkimDNSTokenValue3\`].OutputValue' --output text`
+  echo "CNAME $NAME1 $VALUE1"
+  echo "CNAME $NAME2 $VALUE2"
+  echo "CNAME $NAME3 $VALUE3"
+  echo
+  read -p "Continue? [y/N]: " CONT
+  if [[ "$CONT" != "y" ]]; then
+      exit 1
+  fi
 fi
